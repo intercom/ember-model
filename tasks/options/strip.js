2544232fd7afa5cc36f9b5db78db2daa5cfa5f3c
Pat O'Callaghan <patocallaghan@gmail.com>
2018-03-26 11:11:54 +0100

    Allow grunt build to work

--- tasks/options/strip.js
+++ tasks/options/strip.js
@@ -1,8 +1,8 @@
 module.exports = {
   production : {
     src : 'dist/ember-model.prod.js',
+    dest : 'dist/ember-model.prod.js',
     options : {
-      inline: true,
       nodes : ['Ember.assert']
     }
   }
